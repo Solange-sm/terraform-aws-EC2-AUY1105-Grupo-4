3f615a0d2bd369ede5895aa05166384f09a1873a
Solange-sm <sol.milla@duocuc.cl>
2026-05-28 12:30:57 -0400

    Formatear versions.tf para mayor coherencia

--- versions.tf
+++ versions.tf
@@ -4,12 +4,12 @@
 # ==============================================================================
 
 terraform {
-    required_version =  ">= 1.14.8, < 2.0.0"
+  required_version = ">= 1.14.8, < 2.0.0"
 
-    required_providers {
-        aws = {
-            source  = "hashicorp/aws"
-            version = "~> 5.0"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
+  }
 }
