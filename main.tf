a9ae3d6cbd905dbec99f65a4b2a95d48f974cbaf
Marysabel Aedo Moreno <105820972+MaryAedo@users.noreply.github.com>
2026-05-27 22:04:23 -0400

    modificacion de main para obviar validaciones de checkov que piden semvVer, cambio temporal

--- main.tf
+++ main.tf
@@ -2,17 +2,15 @@
 # Módulo de Cómputo - Evaluación Parcial 2
 # ==============================================================================
 
-# Definición del recurso de cómputo EC2 parametrizado
+# checkov:skip=CKV_AWS_135: La instancia t2.micro no soporta optimización EBS
 resource "aws_instance" "app_iac" {
     ami                    = var.ami_id
     instance_type          = var.instance_type
     
-    # Atributos de red provistos dinámicamente por el Orquestador Raíz
     subnet_id              = var.subnet_id
     vpc_security_group_ids = [var.security_group_id]
     iam_instance_profile   = "LabInstanceProfile"
 
-    # Cumplimiento de normativas de seguridad (Mitigación Checkov EV1)
     monitoring = true
     
     root_block_device {
