d847572efe675a95b193805686c9ac94eff2cad4
Solange-sm <sol.milla@duocuc.cl>
2026-05-28 12:26:41 -0400

    Corrige compatibilidad del pipeline limitando la versión de Terraform en versions.tf

--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@
 # ==============================================================================
 
 terraform {
-    required_version = ">= 1.5.0"
+    required_version =  ">= 1.14.8, < 2.0.0"
 
     required_providers {
         aws = {
